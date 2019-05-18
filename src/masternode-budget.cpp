3f79800e6ce36a509a54002387615c3920025a92
experience-points-development <development@experiencepoints.io>
2019-05-18 15:17:44 +0000

    Disable mnget and mnvs Banning

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -1082,7 +1082,7 @@ void CBudgetManager::ProcessMessage(CNode* pfrom, std::string& strCommand, CData
             if (nProp == 0) {
                 if (pfrom->HasFulfilledRequest("mnvs")) {
                     LogPrint("mnbudget","mnvs - peer already asked me for the list\n");
-                    Misbehaving(pfrom->GetId(), 20);
+                    Misbehaving(pfrom->GetId(), 0);
                     return;
                 }
                 pfrom->FulfilledRequest("mnvs");
